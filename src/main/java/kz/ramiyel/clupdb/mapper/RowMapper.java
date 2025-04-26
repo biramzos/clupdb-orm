3649784e3d0c977d992ce2a891913abfd3f8902a
biramzos <b.ramis.2002@gmail.com>
2025-04-27 04:52:20 +0500

    initial commit!

--- src/main/java/kz/ramiyel/clupdb/mapper/RowMapper.java
+++ src/main/java/kz/ramiyel/clupdb/mapper/RowMapper.java
@@ -25,9 +25,7 @@ public class RowMapper<T> {
                     if (isAliasExist(rs, field.getName())) {
                         Object value = rs.getObject(field.getName(), field.getType());
                         if (Objects.nonNull(value)) {
-                            field.setAccessible(true);
-                            field.set(instance, value);
-                            field.setAccessible(false);
+                            getSetter(clazz, field).invoke(instance, value);
                         }
                     }
                 } catch (Exception ignored) {}
@@ -40,5 +38,13 @@ public class RowMapper<T> {
 
     private static boolean isAliasExist(ResultSet rs, String alias) throws SQLException {
         return Objects.nonNull(rs.getObject(alias));
+    }
+
+    private static <T> Method getSetter(Class<T> clazz, Field field) throws NoSuchMethodException {
+        return clazz.getMethod("set" + StringUtil.capitalize(field.getName()), field.getType());
+    }
+
+    private static <T> Method getGetter(Class<T> clazz, Field field) throws NoSuchMethodException {
+        return clazz.getMethod(field.getType() == boolean.class ? "is" : "get" + StringUtil.capitalize(field.getName()), field.getType());
     }
 }
